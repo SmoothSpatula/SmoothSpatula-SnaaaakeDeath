353bb64ff8e388fcafefc1ba3c2444bda320f1f6
Gabriel Combe-Ounkham <gabrielcombe@gmail.com>
2024-03-31 04:04:59 +0100

    Initial commit

---
+++ main.lua
@@ -0,0 +1,73 @@
+-- Snake Death v1.0.0
+-- SmoothSpatula
+
+log.info("Successfully loaded ".._ENV["!guid"]..".")
+Helper = require("./helper")
+
+-- ========== Audio ==========
+
+local death_sfx = gm.audio_create_stream(_ENV["!plugins_mod_folder_path"].."/SnakeDeath.ogg")
+if death_sfx ~= -1 then log.info("Loaded death sfx.")
+else log.info("Failed to load death sfx.") end
+
+local global_death_sfx = gm.audio_create_stream(_ENV["!plugins_mod_folder_path"].."/snake-snake-snaaaake.ogg")
+if global_death_sfx ~= -1 then log.info("Loaded global death sfx.")
+else log.info("Failed to load global death sfx.") end
+
+-- ========== Utils ==========
+
+-- Compute the euclidian distance between two points
+function distance(x1, y1, x2, y2)
+    local dx = x2 - x1
+    local dy = y2 - y1
+    return math.sqrt ( dx * dx + dy * dy )
+end
+
+-- Compute the norm of a 2D vector
+function norm(x1, y1)
+    return math.sqrt(x1 * x1 + y1 * y1)
+end
+
+-- ========== Main ==========
+
+-- Get the client player instance
+local player = nil
+gm.pre_script_hook(gm.constants.__input_system_tick, function()
+    if player == nil then
+        player = Helper.get_client_player()
+    end
+end)
+
+-- Play a custom sound when a player die and when all the players are dead 
+gm.post_script_hook(gm.constants.actor_set_dead, function(self, other, result, args)
+    if gm.actor_is_player(args[1])  then
+        local player_dead = args[1].value
+
+        local dist = distance(player.x, player.y, player_dead.x, player_dead.y)
+
+        local x_offset = player_dead.x - player.x
+        local y_offset = player_dead.y - player.y
+        
+        if dist > 1000 then
+            local norm = norm(x_offset, y_offset)
+            x_offset = 1000 * x_offset/norm
+            y_offset = 1000 * y_offset/norm
+        end
+
+        -- Play a sound when someone dies
+        gm.sound_play_at(death_sfx, 1, 1, player.x + x_offset, player.y + y_offset, 500000)
+
+        for i = 1, #gm.CInstance.instances_active do
+            local inst = gm.CInstance.instances_active[i]
+            if inst.object_index == gm.constants.oP and gm.actor_is_alive(inst.id) then return end
+        end
+        
+        -- Play a sound when everyone is dead
+        gm.sound_play_global(global_death_sfx, 1, 1)
+    end 
+end)
+
+-- Reset the player when the run is finished
+gm.post_script_hook(gm.constants.run_destroy, function(self, other, result, args)
+    player = nil
+end)
